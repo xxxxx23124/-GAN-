80e4ebe12b2b669d5aaa7421be47338817c0f8cc
Tom Tom <137014884+xxxxx23124@users.noreply.github.com>
2024-05-02 11:51:52 +0800

    Update wgangpsnR2.py

--- train/wgangpsnR2.py
+++ train/wgangpsnR2.py
@@ -91,8 +91,6 @@ class Train(trainunits.Units):
                     {"epoch": f"{epoch}", "Loss_G": f"{g_loss.item():.4f}", "real_loss": f"{real_loss.item():.4f}",
                      "fake_loss": f"{fake_loss.item():.4f}", "r2_reg": f"{r2_reg.item():.4f}",
                      "gp": f"{gp.item():.4f}"})
-                self.scheduler_G.step()
-                self.scheduler_D.step()
                 proc_bar.update(1)
             self.save_ckpt('WGANGPSNR2', epoch + 1, 0)
             proc_bar.reset()
